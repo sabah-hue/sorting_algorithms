a9950aea469608ec0c7313c02afd7825e1efa2d8
sabah-hue <sabah.abdelbaset@gmail.com>
2024-01-23 22:46:19 +0200

    select algorithm

---
+++ 3-quick_sort.c
@@ -0,0 +1,16 @@
+ #include "sort.h"
+
+/**
+ * quick_sort - sorts an array of integers in
+ * ascending order using the Quick sort algorithm
+ *
+ * Description: A function that sorts an array of integers in
+ * ascending order using the Quick sort algorithm
+ * @array: pointer to a array
+ * @size: size of array
+ *
+ * Return: no thing
+ */
+void quick_sort(int *array, size_t size)
+{
+}
